678454e960e357d15648281960af058364fc8fc1
Cryptic Defenders <crypticdefenders@gmail.com>
2024-06-03 13:30:57 +0300

    fixing some bugs in frontend when fetching data from the database and implementing rate limiting in the server

--- frontend/src/functions/fetchDoctors.ts
+++ frontend/src/functions/fetchDoctors.ts
@@ -2,14 +2,10 @@ import { API_URL } from "@/utils/API_URL"
 import axios from "axios"
 
 export const fetchDoctors = async () => {
-    try {
-        const response = await axios.get(`${API_URL}/doctors`, {
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            }
-        });
-        return response.data
-    } catch (error) {
-        return error
-    }
+    const response = await axios.get(`${API_URL}/doctors`, {
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    });
+    return response.data
 }
